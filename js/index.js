7382d8048cabdb42b6cf9fc4fdcecea0cc48b275
Joseph Curley <joseph.curley@velir.com>
2017-07-27 16:30:45 -0400

    Added drag and drop functionality to touch devices.

--- js/index.js
+++ js/index.js
@@ -40,11 +40,28 @@ function bindImageSwapper(){
 		 	//e.preventDefault();
 			$startImage = $(this);
 		})
-		.on("mouseup touchend", ".is-swappable", function(e){
+		.on("mouseup", ".is-swappable", function(e){
+
 			e.preventDefault();
 			$endImage = $(this);
 			//Verify there is a start image and invoke the swapImage function if so.
 			$startImage && swapImage($startImage, $endImage);
+		})
+		.on("touchend", ".is-swappable", function(e){
+			//touch events need to be handled seperately.
+			e.preventDefault();
+			e.stopPropagation();
+
+			//Get the coordinates of the element where the touch event ends.
+			const newLocation = e.changedTouches[0];
+			const endImage = document.elementFromPoint(newLocation.clientX, newLocation.clientY);
+
+			$endImage = $(endImage);
+			//If the new item is a swappable image. Swap them.
+			if ($endImage.hasClass("is-swappable") && $startImage){
+				swapImage($startImage, $endImage);
+			}
+
 		});
 	/*
 		This functionality is true to the spirit of the challenge,
